13d90db3bebea7d1b4c592367b041be57197b59d
HikariEda <meacalmeaca@gmail.com>
2018-06-20 10:48:07 +0900

    GitHubはじめました
    GitHubはじめましたあああ

---
+++ Assets/Script/ItemMenuDisply.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMenuDisply : MonoBehaviour {
+	
+	public GameObject menuCanvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Canvas表示と非表示切り替え
+	public void Changedmenu( bool value ){
+		//Debug.Log (value);
+		if (value) {
+			menuCanvas.SetActive (true);
+		} else {
+			menuCanvas.SetActive (false);
+		}
+	}
+
+	//Canvas非表示
+	public void Closemenu(){
+		menuCanvas.SetActive (false);
+	}
+	//Canvas表示
+	public void Openmenu(){
+		menuCanvas.SetActive (true);	
+	}
+}
---
+++ Assets/Script/NewItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewItem : MonoBehaviour {
+	
+	public GameObject[] installitem;
+	public int number;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void install(int number)
+	{
+		this.number = number;
+		//アイテムを設置
+		Instantiate (installitem[number]);
+	}
+}
